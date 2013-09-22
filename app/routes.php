c051034bc1f635bd9456af9846957820e5e26f83
Christopher E. Walker <cewalker@uchicago.edu>
2013-09-21 23:12:36 -0500

    updated posts to have creator and avatar

--- app/routes.php
+++ app/routes.php
@@ -1,30 +1,69 @@
 <?php
 
 use Desarrolla2\RSSClient\RSSClient;
+date_default_timezone_set('America/Chicago');
 
 // http://planitpurple.northwestern.edu/feed/xml/447
 
-Route::get('posts', function()
-{
+//Route::get('posts', function()
+//{
+//    $url = 'http://studentaffairsnu.wordpress.com/feed/';
+//    $client = new RSSClient();
+//    $client->addFeeds([$url], 'posts');
+//    $items = $client->fetch('posts');
+//
+//    $itemsArray = [];
+//    foreach($items as $item)
+//    {
+//        $title = $item->getTitle();
+//        $link  = $item->getLink();
+//
+//        $date = $item->getPubDate();
+//        $date->setTimezone(new DateTimeZone('America/Chicago'));
+//        $dateString = $date->format('m.d.y');
+//
+//        $author = $item->getAuthor();
+//
+//
+//        $description = preg_replace('/<img.*>/', '', $item->getDescription() );
+//
+//        array_push($itemsArray, [
+//            'title'       => $title,
+//            'link'        => $link,
+//            'description' => $description,
+//            'pubDate'     => $dateString,
+//            'author'      => $author
+//        ]);
+//    }
+//
+//    $firstTwoItems = [$itemsArray[0], $itemsArray[1]];
+//	return Response::json($firstTwoItems);
+//});
+
+Route::get('posts', function() {
     $url = 'http://studentaffairsnu.wordpress.com/feed/';
-    $client = new RSSClient();
-    $client->addFeeds([$url], 'posts');
-    $items = $client->fetch('posts');
+    $rss = simplexml_load_file($url);
 
     $itemsArray = [];
-    foreach($items as $item)
+
+    foreach($rss->channel->item as $item)
     {
-        $title = $item->getTitle();
-        $link  = $item->getLink();
-        $description = preg_replace('/<img.*>/', '', $item->getDescription() );
+        $pubDate = (string) $item->pubDate;
+        $date = new DateTime($pubDate);
+        $date->setTimezone(new DateTimeZone('America/Chicago'));
+        $dateString = $date->format('m.d.y');
+
+        $avatar = (string) $item->children('http://search.yahoo.com/mrss/')->attributes()->url;
 
         array_push($itemsArray, [
-            'title'        => $title,
-            'link'        => $link,
-            'description' => $description
+            'pubDate' => $dateString,
+            'link'    => (string) $item->link,
+            'description' => (string) $item->description,
+            'title'   => (string) $item->title,
+            'creator' => (string) $item->children('http://purl.org/dc/elements/1.1/')->creator,
+            'avatar'  => $avatar
         ]);
     }
-
     $firstTwoItems = [$itemsArray[0], $itemsArray[1]];
-	return Response::json($firstTwoItems);
+    return Response::json($firstTwoItems);
 });
